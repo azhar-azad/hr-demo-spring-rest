9cd7e26b4e0964297fd657b2b2d3b07039935bb6
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-03-15 22:02:25 +0600

    initial commit

---
+++ src/main/java/com/azad/practice/hrdemospringrest/HrDemoSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.azad.practice.hrdemospringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrDemoSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrDemoSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azad/practice/hrdemospringrest/HrDemoSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azad.practice.hrdemospringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrDemoSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
